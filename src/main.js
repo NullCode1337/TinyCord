a88fd75c17e89843aa78aff4cb5a28e2d6290937
NullCode1337 <ishaqahmed.dev@gmail.com>
2025-05-25 10:58:26 +0600

    Initialize webview

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-const { invoke } = window.__TAURI__.core;
+const { Window } = window.__TAURI__.window;
+const { WebviewWindow } = window.__TAURI__.webviewWindow;
+
+const webview = new WebviewWindow("discordMain", {
+  url: 'https://discord.com/channels/@me',
+  width: 1280,
+  height: 720,
+  title: "TinyCord"
+});
+
