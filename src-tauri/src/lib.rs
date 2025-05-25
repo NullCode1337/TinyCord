a3753e75d02628b87304edc056fc2be1c3da0538
NullCode1337 <ishaqahmed.dev@gmail.com>
2025-05-25 16:43:44 +0600

    Fix unintended duplicate

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,8 +19,10 @@ fn exit_app() {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![exit_app])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            exit_app
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
